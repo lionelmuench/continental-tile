0d3db2802dc185f33a9f6cd32334dbace60733f3
Lionel Muench <lionelmuench@gmail.com>
2026-03-26 14:51:26 -0400

    Initial commit for continental-tile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
+
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://continentaltileinc.com',
+  image: {
+    domains: ['images.unsplash.com']
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: cloudflare(),
+  integrations: [sitemap(), partytown()]
+});
